b6bcb26086a3397786fce6f22d568c68ef68e7d6
jshuhler <15068877+jshuhler@users.noreply.github.com>
2025-12-20 17:43:56 -0500

    working to combine palindrome pieces

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,48 +1,41 @@
-// const palindromes = function () {
-
-// };
-
-// initial variables
 let string = "Lid off a daffodil."
 let punctuation = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 
-// lowercase string
-let stringLower = string.toLowerCase();
-
-// string to array
-let arr = stringLower.split("");
-
-// remove punctuation
-let arrNoPunc = arr.filter(char => {
-    return !punctuation.includes(char);
-});
-
-// remove spaces - change arr.filter to arrNoPunc.filter when the above function is ready
-let arrNoSpaces = arr.filter((items) => {
+const palindromes = function () {
+    let stringLower = string.toLowerCase();
+    let arr = stringLower.split("");
+    let arrNoPunc = arr.filter(char => {
+        !punctuation.includes(char);
+    });
+    let arrNoSpaces = arrNoPunc.filter((items) => {
     let spaces = " ";
     return items !== spaces;
-});
+    });
+    let arrNoSpacesReverse = arrNoSpaces.toReversed();
+    let noSpaces = arrNoSpaces.join('');
+    let noSpacesReverse = arrNoSpacesReverse.join('');
+    return noSpaces === noSpacesReverse;
+
+};
+
+// initial variables
+
+// lowercase string
+
+
+// string to array
+
+// remove punctuation
+
+
+// remove spaces - change arr.filter to arrNoPunc.filter when the above function is ready
 
 // reverse arrNoSpaces 
-let arrNoSpacesReverse = arrNoSpaces.toReversed();
 
 // change the arrays back to strings
-let noSpaces = arrNoSpaces.join('');
-let noSpacesReverse = arrNoSpacesReverse.join('');
 
-// check the new strings
-// const palindromes = function (noSpaces,noSpacesReverse) {
-//     if (noSpaces === noSpacesReverse) {
-//         console.log(palindromes);
-//         return true;
-//     } else {
-//         console.log(palindromes);
-//         return false;
-//     };
-// };
 
-const palindromes = function () {
-    return noSpaces === noSpacesReverse;
-};
+// compare the strings
+
 
 palindromes("string");
